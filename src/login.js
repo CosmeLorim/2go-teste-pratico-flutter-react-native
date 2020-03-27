32211cb5dfae446a1b82a1433d9fe86c72849780
Cosme Lorim <cosme.lorim@gmail.com>
2020-03-26 22:32:21 -0400

    Add header page

--- src/login.js
+++ src/login.js
@@ -1,19 +1,34 @@
 import React from 'react'
-import { StyleSheet, Text, View, ImageBackground } from 'react-native'
+import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native'
 
 const imageBackground = require('../assets/images/background.png')
+const logo = require('../assets/images/logo_branca.png')
 
 export const Login = () => {
   return (
     <View style={styles.container}>
       <ImageBackground source={imageBackground} style={styles.imageBackground}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <Image source={logo} />
+          </View>
+          <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+          </View>
+        </View>
       </ImageBackground>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  header: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(71, 71, 71, 0.91)',
+    height: 272,
+    width: '100%',
+  },
   container: {
     flex: 1,
     alignItems: 'center',
@@ -23,6 +38,5 @@ const styles = StyleSheet.create({
     flex: 1,
     width: '100%',
     resizeMode: "cover",
-    justifyContent: "center"
   },
 })
