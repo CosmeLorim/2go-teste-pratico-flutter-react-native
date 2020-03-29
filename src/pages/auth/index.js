4bddfdb84d4b2aa728b9b096acbd7848ea618de2
Cosme Lorim <cosme.lorim@gmail.com>
2020-03-28 23:27:25 -0400

    Improves auth module management

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,1 +1,7 @@
 export { Login } from './Login'
+export { Register } from './Register'
+export { TermsOfuse } from './TermsOfUse'
+export { AuthNavigation } from './AuthNavigation'
+
+import { PageAuth } from './PageAuth'
+export default PageAuth
