df4bfc66e4b9e3ade906fc8347d1face6840718b
Cosme Lorim <cosme.lorim@gmail.com>
2020-03-28 22:44:18 -0400

    Configure primary color via ThemeProvider

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { ThemeProvider } from 'react-native-elements'
 import { App } from './src/app'
+
+const theme = {
+  colors: {
+    primary: '#af3200',
+  }
+}
 
 export default () => {
   return (
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   )
 }
